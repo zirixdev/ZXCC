ef713ac67f9aed5f23fd61be6487b1e5e47060f3
desavera <desavera@gmail.com>
2014-09-25 17:56:36 -0300

    ALERT OK

--- dev/ZX_CC/src/zirix/zxcc/cron/ZXAlerter.java
+++ dev/ZX_CC/src/zirix/zxcc/cron/ZXAlerter.java
@@ -9,6 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Vector;
 import antena.mailer.ADGoogleMailer;
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -26,6 +27,10 @@ public class ZXAlerter {
 	public static String SCHEDED_WORK_FLAG = "0";
 	public static String STARTED_WORK_FLAG = "1";
 	public static String FINISHED_WORK_FLAG = "2";
+
+	public static String ALERT_PENDING = "0";
+	public static String ALERT_TOEXPIRE_SENT = "1";
+	public static String ALERT_EXPIRED_SENT = "2";
 
 	private String username_; 
 	private String password_; 
@@ -52,70 +57,86 @@ public class ZXAlerter {
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/zxccmock?" + "user=zirix&password=pinguim01");
 
 		// SCHEDED_WORKs
-		PreparedStatement stmt0 = con.prepareStatement("SELECT NOW(),sched_time_stamp,restriction_id,work_id,work_name,cod_usuario from sched_work where work_state_id = " + SCHEDED_WORK_FLAG);
-		ResultSet res0 = stmt0.executeQuery();
+		PreparedStatement stmtTOEXPIRE = con.prepareStatement("SELECT NOW(),sched_time_stamp,restriction_id,work_id,work_name,cod_usuario from sched_work where work_state_id = " + SCHEDED_WORK_FLAG + " AND alert_status != " + ALERT_TOEXPIRE_SENT);
+		ResultSet resTOEXPIRE = stmtTOEXPIRE.executeQuery();
 
 		// STARTED_WORKs
-		PreparedStatement stmt1 = con.prepareStatement("SELECT NOW(),sched_time_stamp,restriction_id,work_id,work_name,cod_usuario from sched_work where work_state_id = " + STARTED_WORK_FLAG);
-		ResultSet res1 = stmt1.executeQuery();
+		PreparedStatement stmtEXPIRED = con.prepareStatement("SELECT NOW(),sched_time_stamp,restriction_id,work_id,work_name,cod_usuario from sched_work where work_state_id = " + STARTED_WORK_FLAG + " AND alert_status != " + ALERT_EXPIRED_SENT);
+		ResultSet resEXPIRED = stmtEXPIRED.executeQuery();
 
-		while (res0.next()) {
+		while (resTOEXPIRE.next()) {
 
-			String now = res0.getString(1);
+			String now = resTOEXPIRE.getString(1);
 			Timestamp now_time = Timestamp.valueOf(now);
 
-			String resctriction_id = res0.getString(3);
+			String resctriction_id = resTOEXPIRE.getString(3);
 
 			PreparedStatement stmt2 = con.prepareStatement("SELECT restriction_value from restriction_work where restriction_id = " + resctriction_id);
 			ResultSet res2 = stmt2.executeQuery();
 
-			Timestamp sched_time = res0.getTimestamp(2);
-			Integer work_id = res0.getInt(4);
-			String work_name = res0.getString(5);
-			Integer cod_usuario = res0.getInt(6);
+			Timestamp sched_time = resTOEXPIRE.getTimestamp(2);
+			Integer work_id = resTOEXPIRE.getInt(4);
+			String work_name = resTOEXPIRE.getString(5);
+			Integer cod_usuario = resTOEXPIRE.getInt(6);
 
 			res2.next();
 			Time restriction_val = res2.getTime(1);
 
-			if (alerter.eval0(now_time,sched_time,restriction_val)) {
+			if (alerter.evalTOEXPIRE(now_time,sched_time,restriction_val)) {
 
-				alerter.notify(work_id,work_name,cod_usuario,RISK_SUBJECT);
+				PreparedStatement stmt3 = con.prepareStatement("SELECT email from user_group where user_group_id in (SELECT user_group_id from alert_group where alert_group_id = (SELECT alert_group_id from work_alert where work_alert_id = (SELECT work_alert_id from sched_work where work_id =? )))");
+				stmt3.setInt(1,work_id);
+				ResultSet res3 = stmt3.executeQuery();
+
+				Vector<String> email_TO_List = new Vector<String>();
+				while (res3.next()) 
+					email_TO_List.add(res3.getString(1));	
+
+				alerter.notify(work_id,work_name,cod_usuario,RISK_SUBJECT,email_TO_List);
 				
-				PreparedStatement stmt3 = con.prepareStatement("UPDATE sched_work set alert_status = 1 where work_id =?");
-				stmt3.setInt(1,work_id);
-				stmt3.executeUpdate();
+				PreparedStatement stmt4 = con.prepareStatement("UPDATE sched_work set alert_status =? where work_id =?");
+				stmt4.setInt(1,Integer.parseInt(ALERT_TOEXPIRE_SENT));
+				stmt4.setInt(2,work_id);
+				stmt4.executeUpdate();
 			}
 		}
 
-		while (res1.next()) {
+		while (resEXPIRED.next()) {
 
-			String now = res1.getString(1);
+			String now = resEXPIRED.getString(1);
 			Timestamp now_time = Timestamp.valueOf(now);
 
-			String resctriction_id = res1.getString(3);
+			String resctriction_id = resEXPIRED.getString(3);
 
 			PreparedStatement stmt2 = con.prepareStatement("SELECT restriction_value from restriction_work where restriction_id = " + resctriction_id);
 			ResultSet res2 = stmt2.executeQuery();
 
-			Timestamp sched_time = res1.getTimestamp(2);
-			Integer work_id = res1.getInt(4);
-			String work_name = res1.getString(5);
-			Integer cod_usuario = res1.getInt(6);
+			Timestamp sched_time = resEXPIRED.getTimestamp(2);
+			Integer work_id = resEXPIRED.getInt(4);
+			String work_name = resEXPIRED.getString(5);
+			Integer cod_usuario = resEXPIRED.getInt(6);
 
 			res2.next();
 			Time restriction_val = res2.getTime(1);
 
-			if (alerter.eval1(now_time,sched_time,restriction_val)) {
+			if (alerter.evalEXPIRED(now_time,sched_time,restriction_val)) {
 
-				alerter.notify(work_id,work_name,cod_usuario,OVERTIME_SUBJECT);
-
-				PreparedStatement stmt3 = con.prepareStatement("UPDATE sched_work set alert_status = 1 where work_id =?");
+				PreparedStatement stmt3 = con.prepareStatement("SELECT email from user_group where user_group_id in (SELECT user_group_id from alert_group where alert_group_id = (SELECT alert_group_id from work_alert where work_alert_id = (SELECT work_alert_id from sched_work where work_id =? )))");
 				stmt3.setInt(1,work_id);
-				stmt3.executeUpdate();
+				ResultSet res3 = stmt3.executeQuery();
+
+				Vector<String> email_TO_List = new Vector<String>();
+				while (res3.next()) 
+					email_TO_List.add(res3.getString(1));	
+
+				alerter.notify(work_id,work_name,cod_usuario,OVERTIME_SUBJECT,email_TO_List);
+
+				PreparedStatement stmt4 = con.prepareStatement("UPDATE sched_work set alert_status =? where work_id =?");
+				stmt4.setInt(1,Integer.parseInt(ALERT_EXPIRED_SENT));
+				stmt4.setInt(2,work_id);
+				stmt4.executeUpdate();
 			}
 		}
-
-
 
 	  } catch(Exception ex) {
 
@@ -134,7 +155,7 @@ public class ZXAlerter {
 	/*
 	 * avalia o cenario onde a tarefa se aproxima de 75% do tempo limite e ainda nao foi iniciada
 	 */
-	public boolean eval0(Timestamp now,Timestamp sched,Time rest) {
+	public boolean evalTOEXPIRE(Timestamp now,Timestamp sched,Time rest) {
 
 		long sched_millis = sched.getTime();
 
@@ -160,7 +181,7 @@ public class ZXAlerter {
 	/*
 	 * avalia o cenario onde a tarefa nao foi concluida no tempo limite
 	 */
-	public boolean eval1(Timestamp now,Timestamp sched,Time rest) {
+	public boolean evalEXPIRED(Timestamp now,Timestamp sched,Time rest) {
 
 		long sched_millis = sched.getTime();
 		long rest_millis = rest.getTime();
@@ -178,12 +199,14 @@ public class ZXAlerter {
 	/*
 	 * envia e-mail de notificacao com o subject em questao
 	 */
-	public void notify(Integer work_id,String work_name,Integer cod_usuario,String subject) throws MessagingException {
+	public void notify(Integer work_id,String work_name,Integer cod_usuario,String subject,Vector<String> email_TO_List) throws MessagingException {
 
 		ADGoogleMailer mailer = new ADGoogleMailer(username_,password_);
 
 		String msg = "ZXCC MAILER : " + work_id + " | " + work_name + " | " + cod_usuario;
-		mailer.send("desavera@gmail.com",msg,subject);
+
+		for (int i=0;i < email_TO_List.size();i++)
+			mailer.send(email_TO_List.elementAt(i),msg,subject);
 
 	}
 }
