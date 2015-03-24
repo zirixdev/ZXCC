564f1ff205bcb36ab868d361e19464548c735f45
rbmarques <raphael.zirix@gmail.com>
2015-03-24 16:24:35 -0300

    24-03-2015

--- bin/alerter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-/usr/bin/touch /tmp/lixo
-/usr/bin/java -jar ~/Lib/zxccalerter_beta-0-3.jar jdbc:mysql://localhost/ZX_CC_QA? user=zirix&password=pinguim01  zxcc@zirix.com.br zxcc123/*
-
